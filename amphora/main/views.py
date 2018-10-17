d021a9333c74164717f8a9e68be22c6a8b30fec8
Unknown <vero@nuagesdencre.com>
2018-10-17 13:19:56 +0100

    added basic structure to base.html + link to materialize.css/js

--- amphora/main/views.py
+++ amphora/main/views.py
@@ -3,4 +3,12 @@ from flask import render_template, request, Blueprint, abort
 
 main = Blueprint('main', __name__)
 
-@main.route('/'):
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+
+@main.route('/contact')
+def contact():
+    return render_template('contact.html')
