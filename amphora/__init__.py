7f54e6445c3a95c4c027b3f2efef6af385a113cb
Unknown <vero@nuagesdencre.com>
2018-10-17 15:17:39 +0100

    registering error_pages blueprint

--- amphora/__init__.py
+++ amphora/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from amphora.main.views import main
-
+from amphora.error.handlers import error_pages
 
 app = Flask(__name__)
 
+# registering blueprints
 app.register_blueprint(main)
+app.register_blueprint(error_pages)
