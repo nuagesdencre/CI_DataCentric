a6a2e0880e0949fcb30b5de330d669d6389fd4e9
Unknown <vero@nuagesdencre.com>
2018-10-19 14:21:24 +0100

    register form with validation + check if username/email are unique

--- amphora/users/forms.py
+++ amphora/users/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo,Length
 # DataRequired vs InputRequired.... to check
 
 from flask_login import current_user
@@ -16,8 +16,17 @@ class Login(FlaskForm):
 
 class Register(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6,
+                                                                            max=30, message='Minimum 6 characters'),
+                                                     EqualTo('psw_again')])
     psw_again = PasswordField('Confirm Password', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    submit = SubmitField('Log in!')
+    submit = SubmitField('Register now!')
 
+    def unique_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email has already been registered!')
+
+    def unique_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username has already been registered!')
