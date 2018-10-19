22cc1d448f95d3ab3515b0f84a64625e92e2bf1f
Unknown <vero@nuagesdencre.com>
2018-10-19 13:36:27 +0100

    trying to set up story and being tables with relationships...
    https://docs.sqlalchemy.org/en/latest/orm/basic_relationships.html

--- amphora/models.py
+++ amphora/models.py
@@ -19,6 +19,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(72), unique=True, index=True)
     psw_hash = db.Column(db.String(128))
     profile_pic = db.Column(db.String(72), nullable=False, default='amphora_profile.png')
+    # relationships
     stories = db.relationship('Story', backref='author', lazy=True)
     beings = db.relationship('Being', backref='author', lazy=True)
 
@@ -42,10 +43,40 @@ class Story():
 
     __tablename__ = 'stories'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(36), nullable=False, unique=True, index=True)
+    title = db.Column(db.String(60), nullable=False, unique=True, index=True)
+    country = db.Column(db.String(60), nullable=False, index=True)
+    text = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(60), index=True)
+    # relationships
+    users = db.relationship('User', backref='user', lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
+    def __init__(self, title, country, text, category):
+        self.title = title
+        self.country = country
+        self.text = text
+        self.category = category
+
+    def __repr__(self):
+        return "Story title: {}".format(self.title)
 
 class Being():
+
     __tablename__ = 'beings'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(36), nullable=False, unique=True, index=True)
+    name = db.Column(db.String(60), nullable=False, unique=True, index=True)
+    country = db.Column(db.String(60), nullable=False, index=True)
+    text = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(60), index=True)
+    # relationships
+    users = db.relationship('User', backref='user', lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, name, country, text, category):
+        self.name = name
+        self.country = country
+        self.text = text
+        self.category = category
+
+    def __repr__(self):
+        return "Being name: {}".format(self.name)
