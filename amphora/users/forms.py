eeac1b7b025e538ef4b87a1d73c750b018735f63
Unknown <vero@nuagesdencre.com>
2018-10-24 16:35:31 +0100

    corrected validation bug - problem with updating acct screen (wip)

--- amphora/users/forms.py
+++ amphora/users/forms.py
@@ -1,3 +1,4 @@
+from sqlalchemy.exc import IntegrityError
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed, FileField
 from wtforms import StringField, PasswordField, SubmitField
@@ -8,15 +9,18 @@ from amphora.models import User
 
 
 class ValidationMixin:
- # fix validation and error messages
 
-    def unique_email(self, field):
-        if User.query.filter_by(email=field.data).first():
-            raise ValidationError('Email has already been registered!')
+    # making sure the username and email address are not already in the user table
 
-    def unique_username(self, field):
-        if User.query.filter_by(username=field.data).first():
-            raise ValidationError('Username has already been registered!')
+    def validate_email(self, field):
+        user = User.query.filter_by(email=field.data).first()
+        if user is not None:
+            raise ValidationError('This email address has already been registered.')
+
+    def validate_username(self, field):
+        user = User.query.filter_by(username=field.data).first()
+        if user is not None:
+            raise ValidationError('This username is already taken.')
 
 
 class Login(FlaskForm):
@@ -35,7 +39,7 @@ class Register(FlaskForm, ValidationMixin):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6,
                                                                             max=30, message='Minimum 6 characters'),
-                                                     EqualTo('psw_again', message='Password does not match.')])
+                                                     EqualTo('psw_again', message='The passwords do not match.')])
     psw_again = PasswordField('Confirm Password', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Register now!')
