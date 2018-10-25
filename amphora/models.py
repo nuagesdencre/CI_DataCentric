f34d6e4ee9a56adc82ee556ea6325e68ebde85e3
Unknown <vero@nuagesdencre.com>
2018-10-25 14:17:48 +0100

    updating model with new comments + psw reset methods
    set_password, psw_check. get_rest_password_token, verify_reset_password_token

--- amphora/models.py
+++ amphora/models.py
@@ -1,17 +1,21 @@
-from amphora import db, login_manager
+from amphora import db, login_manager, app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
-
+from time import time
+import jwt
 
 
 @login_manager.user_loader
 def load_user(user_id):
+    """
+    Reload the user object from the user ID stored in the session
+    """
     return User.query.get(user_id)
 
 
 class User(db.Model, UserMixin):
-    __tablename__= 'users'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(36), nullable=False, unique=True, index=True)
     email = db.Column(db.String(72), unique=True, index=True)
@@ -22,41 +26,82 @@ class User(db.Model, UserMixin):
     # One author for many stories & beings
     beings = db.relationship('Being', backref='user', lazy=True)
 
-    # user set up
+
     def __init__(self, username, email, psw):
+        """
+        Initialize user object
+        """
         self.username = username
         self.email = email
         self.psw_hash = generate_password_hash(psw)
 
-    # user self-representation
     def __repr__(self):
+        """
+        Set user self-representation
+        """
         return "Username {}".format(self.username)
+
+
+    def set_password(self, psw):
+        """
+        Set a new password (useful for psw reset)
+        """
+        self.psw_hash = generate_password_hash(psw)
 
     # password check
     def psw_check(self, psw):
+        """
+        Check if the password hash of the inputted password
+        matches the hash on file
+        """
         return check_password_hash(self.psw_hash, psw)
 
-    # user avatar using gravater
+    #
+    def get_reset_password_token(self, expires_in=600):
+        """
+        Generate a JWT token as a string following a password reset request
+        """
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        """
+        Decode the JWT token associated to the password reset request
+        """
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
     def avatar(self, size):
+        """
+        Import user avatars using gravater
+        """
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
 
 
 class Story(db.Model):
-
     __tablename__ = 'stories'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60), nullable=False, unique=True, index=True)
     country = db.Column(db.String(60), nullable=False)
     text = db.Column(db.Text)
     category = db.Column(db.String(60), index=True)
-    source = db.Column(db.String(100),  default='None provided')
+    source = db.Column(db.String(100), default='None provided')
     # relationships
     users = db.relationship('User')
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     def __init__(self, title, text, country, category, source, user_id):
+        """
+        Initialize story object
+        """
         self.title = title
         self.text = text
         self.country = country
@@ -65,11 +110,13 @@ class Story(db.Model):
         self.user_id = user_id
 
     def __repr__(self):
+        """
+        Set story self-representation
+        """
         return "Story title: {}".format(self.title)
 
 
 class Being(db.Model):
-
     __tablename__ = 'beings'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), nullable=False, unique=True, index=True)
@@ -77,12 +124,15 @@ class Being(db.Model):
     text = db.Column(db.Text)
     category = db.Column(db.String(60), index=True)
     picture = db.Column(db.String(72), default='amphora_default.png')
-    source = db.Column(db.String(100),  default='None provided')
+    source = db.Column(db.String(100), default='None provided')
     # relationships
     users = db.relationship('User')
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    def __init__(self, name, text, country, category,source, user_id):
+    def __init__(self, name, text, country, category, source, user_id):
+        """
+        Initialize being object
+        """
         self.name = name
         self.text = text
         self.country = country
@@ -91,4 +141,7 @@ class Being(db.Model):
         self.user_id = user_id
 
     def __repr__(self):
+        """
+        Set being self-representation
+        """
         return "Being name: {}".format(self.name)
