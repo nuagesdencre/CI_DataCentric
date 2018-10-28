e06496290d9d7cde644f478f263bbac24918116b
Unknown <vero@nuagesdencre.com>
2018-10-28 21:49:33 +0000

    fixing search bugs

--- amphora/search/forms.py
+++ amphora/search/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
-from wtforms import ValidationError
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 class SearchForm(FlaskForm):
