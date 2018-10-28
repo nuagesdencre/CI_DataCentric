1a2b014b8f39c1cd1d877e12b56e792d7f7d944a
Unknown <vero@nuagesdencre.com>
2018-10-28 21:16:35 +0000

    search filters functional

--- amphora/search/views.py
+++ amphora/search/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, Blueprint,request,redirect, url_for
 from amphora import db
-
+from sqlalchemy import or_
 from amphora.models import User, Category, Story, Being
 from amphora.search.forms import SearchForm
 
@@ -22,9 +22,9 @@ def search_req(query):
     """
      Query database
      """
-    categories = Category.query.filter_by(query=query)
-    stories = Story.query.filter_by(query=query)
-    beings = Being.query.filter_by(query=query)
-    users = User.query.filter_by(query=query)
+    categories = Category.query.filter(or_(Category.name.contains(query), Category.description.contains(query)))
+    stories = Story.query.filter(or_(Story.title.contains(query), Story.meaning.contains(query), Story.text.contains(query)))
+    beings = Being.query.filter(or_(Being.name.contains(query), Being.meaning.contains(query), Being.text.contains(query)))
+    users = User.query.filter(or_(User.username.contains(query), User.email.contains(query)))
     return render_template('search/results.html', stories=stories, beings=beings,
                            categories=categories, users=users)
